ddd0c7f7ac33e83f6aa8dd41a71bad37e4ff6844
Carsten Götzinger <carsten.goetzinger@concedro.com>
2023-03-31 16:11:26 +0200

    test(options): add test for new options
    New options:
    - add-host
    - cap-add
    - cap-drop
    - device
    - dns
    - env
    - env-file
    - hostname
    - label
    - mac-address
    - name
    - restart
    - security-opt
    - tmpfs
    - init
    - privileged

--- test/composerize.test.ts
+++ test/composerize.test.ts
@@ -35,3 +35,25 @@ test('can publish ports', async () => {
   const expected = YAML.load(path.join(__dirname, './data/ports.yaml'));
   expect(result.yaml).toEqual(YAML.stringify(expected, 9, 4));
 });
+
+test('test a complex command', async () => {
+  const cmd = `docker run -p 80:80 --tty --init --security-opt seccomp:unconfined --mac-address 02:42:ac:11:65:43 --ulimit nofile=1024:1024  --network network  -h containerhostname --name nginxc --cap-drop NET_ADMIN  --cap-drop SYS_ADMIN --cap-add ALL  --tmpfs /run --privileged --restart on-failure --device /dev/sdc:/dev/xvdc -l my-label --label com.example.foo=bar -e MYVAR1 --env MYVAR2=foo --env-file ./env.list --add-host somehost:162.242.195.82 --dns 10.0.0.10  --dns=1.1.1.1 -v /var/run/docker.sock:/tmp/docker.sock -v .:/opt --restart always --log-opt max-size=1g nginx`;
+  const result = composerize(cmd);
+  const expected = YAML.load(path.join(__dirname, './data/complex.yaml'));
+  expect(result.yaml).toEqual(YAML.stringify(expected, 9, 4));
+
+  const ulimit = result.messages.find(
+    (msg) => msg.type === MessageType.notImplemented && msg.value.includes('--ulimit')
+  );
+  expect(ulimit).toBeDefined();
+
+  const network = result.messages.find(
+    (msg) => msg.type === MessageType.notImplemented && msg.value.includes('--network')
+  );
+  expect(network).toBeDefined();
+
+  const logOpt = result.messages.find(
+    (msg) => msg.type === MessageType.notImplemented && msg.value.includes('--log-opt')
+  );
+  expect(logOpt).toBeDefined();
+});
