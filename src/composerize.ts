51405c47405412203092463b8d37f37f41024a5f
pricootz <paul_pricootz@yahoo.it>
2024-08-25 10:31:29 +0200

    Updated code to not display "version"
    Updated code to not display "version"

--- src/composerize.ts
+++ src/composerize.ts
@@ -5,25 +5,20 @@ import { deepmerge } from 'deepmerge-ts';
 import { getSupportedOptions } from './options';
 
 const createComposeObjectStructure = (parseResult: ParseResult, composeVersion: number): object => {
-  let composeSpecification = {};
-  composeSpecification = deepmerge(composeSpecification, {
-    version: (Math.floor(composeVersion * 10) / 10).toString(),
+  let composeSpecification = {
     services: {
       [parseResult.serviceName]: {},
     },
-  });
-
+  };
   let service = {};
   parseResult.properties.forEach((result) => (service = deepmerge(result.value, service)));
   // @ts-ignore
   composeSpecification['services'][parseResult.serviceName] = service;
-
   if (parseResult.additionalComposeObjects !== undefined) {
     parseResult.additionalComposeObjects.forEach(
       (obj: object) => (composeSpecification = deepmerge(composeSpecification, obj))
     );
   }
-
   return composeSpecification;
 };
 
@@ -36,16 +31,18 @@ const createComposeObjectStructure = (parseResult: ParseResult, composeVersion: 
 export const composerize = (
   command: string,
   composeVersion: number = 3.9,
-  debug: boolean = false
+  debug: boolean = false,
+  includeVersion: boolean = false
 ): ComposerizeResult => {
   const parseResult = parse(command, debug);
-
   if (debug) {
     console.log('Parse result:');
     console.log(JSON.stringify(parseResult, null, 2));
   }
-
   const composeSpecification = createComposeObjectStructure(parseResult, composeVersion);
+  if (includeVersion) {
+    composeSpecification['version'] = (Math.floor(composeVersion * 10) / 10).toString();
+  }
   return new ComposerizeResult(YAML.stringify(composeSpecification, 9, 4), parseResult.messages);
 };
 
